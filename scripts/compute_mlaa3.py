09a5aca8d6501248ac1916d8635549d54c83db12
Clémentine Phung <clementine@phung.fr>
2025-03-21 15:54:29 +0100

    updated unet

---
+++ scripts/compute_mlaa3.py
@@ -0,0 +1,110 @@
+"""
+Generate MLAA reconstructions (tof)
+"""
+
+import os
+import sys
+from tqdm import tqdm
+import numpy as np
+import pickle as pkl
+import dotenv
+
+import torch
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+dotenv.load_dotenv()
+
+from src.algos.mlaa import MLAA
+from src.utils.projector import Projector
+from src.utils.data.image_dataset import ImageDataset
+
+def save_image(lambda_mlaa, mu_mlaa, save_path, filename):
+    full_save_path = os.path.join(save_path, filename)
+    stacked = torch.stack([lambda_mlaa, mu_mlaa]).cpu().numpy()
+    np.save(full_save_path, stacked)
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    save_path_mlaa = os.environ['PATH_MLAA']
+    path_data = os.environ['PATH_REFERENCE']
+
+    if not os.path.exists(save_path_mlaa):
+        os.makedirs(save_path_mlaa)
+        print(f"Created directory {save_path_mlaa}")
+
+    path_id_patients = os.environ['PATH_ID_PATIENTS']
+    with open(path_id_patients, 'rb') as f:
+        id_patients = pkl.load(f)
+    id_patients = id_patients['train']
+    #id_patients = id_patients['train_valid']
+    print(len(id_patients))
+
+    dataset = ImageDataset(path_data=path_data,
+                           nb_slices_volume=64,
+                           id_patients=id_patients[200:])
+    
+    # initializations
+    projector = Projector(use_tof=False, use_res_model=True)
+    tof_projector = Projector(use_tof=True, use_res_model=True)
+
+    tau = 0.0001
+    bckg_pet = 0
+    use_tof = True
+    
+    mlaa = MLAA(projector=projector,
+                tof_projector=tof_projector,
+                is_tof=use_tof)
+    n_iter = 50
+
+    clip_act = 100000
+    clip_atn = 0.025
+
+    # initialize mu map
+    mu_init = torch.full((64, 256, 256), 0.01) # initialize to water LAC in mm-1
+
+
+    for id_patient in tqdm(range(len(dataset)), ncols=100):
+
+        filename, image = dataset[id_patient]
+
+        if filename in os.listdir(save_path_mlaa):
+            continue
+
+        pet_img = image[0].to(device)
+        mu_img = image[1].to(device)
+
+        pet_img = torch.clamp(pet_img, min=0, max=clip_act)
+        mu_img = torch.clamp(mu_img, min=0, max=clip_atn)
+
+        # compute projection
+
+        # compute attenuation sinogram
+        x_att_fwd = projector.transform(mu_img)
+        att_sino = torch.exp(-x_att_fwd)
+
+        if use_tof:
+            proj_act = tof_projector.transform(pet_img)
+            att_sino = att_sino.unsqueeze(-1).repeat(1, 1, 1, proj_act.shape[-1])
+        else:
+            proj_act = projector.transform(pet_img)
+
+        # forward project with resolution and attenuation model
+        y = torch.clamp(tau * (att_sino * proj_act + bckg_pet), min=0)
+        ybar = torch.poisson(y)
+
+        # solve mlaa
+        lambda_mlaa, mu_mlaa = mlaa.solve(y=ybar,
+                                          mu_init=mu_init,
+                                          tau=tau,
+                                          bckg_pet=bckg_pet,
+                                          n_iter=n_iter,
+                                          display=False)
+        
+        # save reconstruction
+        save_image(lambda_mlaa, mu_mlaa, save_path_mlaa, filename)
+
+
+if __name__=="__main__":
+    main()
--- src/models/unet_old.py
+++ src/models/unet_old.py
@@ -1,3 +1,6 @@
+"""Hwang et al., 2019
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,21 +22,8 @@ class ConvBlock(nn.Module):
         return self.block(x)
 
 
-class DownBlock(nn.Module):
-    """Resolution reduction was performed by 2 × 2 × 2 convolution kernels with stride 2"""
-    def __init__(self, in_channels, out_channels):
-        super(DownBlock, self).__init__()
-        self.block = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, kernel_size=2, stride=2),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.block(x)
-
-
 class UNet(nn.Module):
-    def __init__(self, in_channels=2, out_channels=1, base_filters=64):
+    def __init__(self, in_channels=2, out_channels=1, base_filters=12):
         super(UNet, self).__init__()
         
         # initial merging layer
@@ -41,25 +31,15 @@ class UNet(nn.Module):
         
         # contracting path (ensure the number of filters double at each step)
         self.enc1 = ConvBlock(base_filters, base_filters)
-        self.down1 = DownBlock(base_filters, base_filters)
-
         self.enc2 = ConvBlock(base_filters, base_filters * 2)
-        self.down2 = DownBlock(base_filters * 2, base_filters * 2)
-
         self.enc3 = ConvBlock(base_filters * 2, base_filters * 4)
-        self.down3 = DownBlock(base_filters * 4, base_filters * 4)
-
-        self.enc4 = ConvBlock(base_filters * 4, base_filters * 8)
-        self.down4 = DownBlock(base_filters * 8, base_filters * 8)
+        
+        self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
         
         # bottleneck
-        self.bottleneck = ConvBlock(base_filters * 8, base_filters * 16)
-        self.dropout = nn.Dropout(p=0.15)
-
-        # expanding path
-        self.up4 = nn.ConvTranspose3d(base_filters * 16, base_filters * 8, kernel_size=2, stride=2)
-        self.dec4 = ConvBlock(base_filters * 16, base_filters * 8)
+        self.bottleneck = ConvBlock(base_filters * 4, base_filters * 8)
         
+        # expanding path
         self.up3 = nn.ConvTranspose3d(base_filters * 8, base_filters * 4, kernel_size=2, stride=2)
         self.dec3 = ConvBlock(base_filters * 8, base_filters * 4)
         
@@ -76,14 +56,12 @@ class UNet(nn.Module):
         x = self.initial(x)
         
         e1 = self.enc1(x)
-        e2 = self.enc2(self.down1(e1))
-        e3 = self.enc3(self.down2(e2))
-        e4 = self.enc4(self.down3(e3))
+        e2 = self.enc2(self.pool(e1))
+        e3 = self.enc3(self.pool(e2))
         
-        b = self.dropout(self.bottleneck(self.down4(e4)))
+        b = self.bottleneck(self.pool(e3))
         
-        d4 = self.dec4(torch.cat([self.up4(b), e4], dim=1))
-        d3 = self.dec3(torch.cat([self.up3(d4), e3], dim=1))
+        d3 = self.dec3(torch.cat([self.up3(b), e3], dim=1))
         d2 = self.dec2(torch.cat([self.up2(d3), e2], dim=1))
         d1 = self.dec1(torch.cat([self.up1(d2), e1], dim=1))
         
@@ -96,10 +74,7 @@ if __name__ == "__main__":
 
     model = UNet()
 
-    x = torch.randn(8, 2, 32, 64, 64)  # batch size of 8, 2 input channels, 32x64x64 volume
+    x = torch.randn(8, 2, 32, 32, 32)  # batch size of 8, 2 input channels, 32x32x32 volume
 
     y = model(x)
-    print("out shape", y.shape)  # output shape: (8, 1, 32, 64, 64)
-
-    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"Nb of params: {pytorch_total_params}\n")
+    print(y.shape)  # output shape: (8, 1, 32, 32, 32)
